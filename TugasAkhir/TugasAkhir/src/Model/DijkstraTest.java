2f2a2b4fce2177810639dcd3d01588b4e78aaab3
Diskaandini <andinidiska@gmail.com>
2018-01-01 17:19:23 +0700

    Update DijkstraTest.java

--- TugasAkhir/TugasAkhir/src/Model/DijkstraTest.java
+++ TugasAkhir/TugasAkhir/src/Model/DijkstraTest.java
@@ -7,33 +7,48 @@ package Model;
 
 import Connection.Database;
 import View.App;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Map;
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.EdgeFactory;
+import org.graphstream.graph.EdgeRejectedException;
+import org.graphstream.graph.ElementNotFoundException;
 import org.miv.mbox.Test;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.IdAlreadyInUseException;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.NodeFactory;
+import org.graphstream.stream.AttributeSink;
+import org.graphstream.stream.ElementSink;
+import org.graphstream.stream.GraphParseException;
+import org.graphstream.stream.Sink;
+import org.graphstream.stream.file.FileSink;
+import org.graphstream.stream.file.FileSource;
+import org.graphstream.ui.swingViewer.Viewer;
 
 /**
  *
  * @author VELIA
  */
 public class DijkstraTest {
+    private Graph graph;
     private ArrayList<Vertex> nodes;
     private ArrayList<Segments> segment; 
     Connection conn = null;
     ResultSet rs ;
     PreparedStatement pstmt;
    
-    public String testExcute(App app ) throws SQLException, ClassNotFoundException {
-        nodes = new ArrayList<>();                
-        segment = new ArrayList<Segments>();   
-        
-        for (int i = 0; i < segment.size(); i++) {
-            Vertex location = new Vertex(" "+ i);
-            nodes.add(location);
-        }
+    public String testExcute(App app, String room_code) throws SQLException, ClassNotFoundException {
+        segment = new ArrayList<Segments>();       
         
         String sql = "select * from segments";
         conn = Database.ConnectDB();
@@ -44,22 +59,26 @@ public class DijkstraTest {
             Vertex destionation = new Vertex(rs.getString("Destination"));
             Segments sgm = new Segments(rs.getString("No"), source, destionation, rs.getDouble("Length"));
             segment.add(sgm);
-        }     
+        }
         
-       
-        // check from location qp to destination
+        // check from location qp to destination    
+               
         Graph graph = new Graph(nodes, segment);
-        DijkstraAlgorithm dijkstra = new DijkstraAlgorithm(graph);
-        dijkstra.execute(nodes.get(app.getjComboYourLocation().getSelectedIndex()));
-        LinkedList<Vertex> path = dijkstra.getPath(nodes.get(app.getjComboDestinationObject().getSelectedIndex())); //get destination
+//        DijkstraAlgorithm dijkstra = new DijkstraAlgorithm(graph);
+//        dijkstra.execute(nodes.get(app.getjComboYourLocation().getSelectedIndex()));
+//        dijkstra.execute(nodes.get(0)); //get qp
+//        LinkedList<Vertex> path = dijkstra.getPath(nodes.get(11)); //get destination
      
         String isiReturn = "";
         
-        for (int i = 0; i < path.size()-1; i++) {
-            isiReturn += path.get(i).getId()+ "->" + path.get(i+1).getId() + "\n";
+        for (int i = 0; i < segment.size(); i++) {
+            isiReturn += segment.get(i).getSource().getId()+ "->" + segment.get(i).getDestination().getId() + "\n";
         }
-        
-        return isiReturn;   
-    }         
-    
+
+        return isiReturn;
+    }
+
+    public String testExcute(App aThis) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
